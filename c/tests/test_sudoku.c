7183ead4c36908f286a4003232f6233189867f15
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-08-13 06:53:41 +0200

    Refactor boards directory to include expected/solved boards

--- c/tests/test_sudoku.c
+++ c/tests/test_sudoku.c
@@ -11,7 +11,7 @@ void tearDown(void) {
 }
 
 void test_read_board() {
-  int test_board[9][9] = {
+  int expected_board[9][9] = {
       {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9},
       {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9},
       {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9},
@@ -20,19 +20,22 @@ void test_read_board() {
   };
 
   int n = 9;
-  int **board = sudoku_read_board("tests/fixtures/mock_board.csv");
+  int** board = sudoku_read_board("tests/fixtures/mock_board.csv");
 
   for (int i = 0; i < n; i++) {
-    TEST_ASSERT_EQUAL_INT_ARRAY(test_board[i], board[i], n);
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected_board[i], board[i], n);
   }
 
   array_delete_2d(board, n, n);
 }
 
 void test_sudoku() {
-  // TODO
+  int n = 9;
+  int** solved_board = sudoku_solve(input_board, n);
 
-  TEST_ASSERT_EQUAL_INT(-1, -1);
+  for (int i = 0; i < n; i++) {
+    TEST_ASSERT_EQUAL_INT_ARRAY(expected_board[i], solved_board[i], n);
+  }
 }
 
 int main(void) {
