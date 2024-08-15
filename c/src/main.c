02f343f255953d7b7f0c510dd1c34d37902879de
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-08-15 07:58:59 +0200

    Add TODOs for the CLI in C

--- c/src/main.c
+++ c/src/main.c
@@ -1,5 +1,8 @@
 #include "sudoku.h"
 
+// TODO: Add logging
+// TODO: Raise an error if input file does not exist
+
 int main(int argc, char* argv[]) {
   char* filepath = argv[1];
   int** input_board = sudoku_read_board(filepath);
